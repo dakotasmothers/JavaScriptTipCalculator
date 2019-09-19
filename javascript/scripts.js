5c347e896074515738d7e1f14b27ce50e347eaba
Dakota Smothers <dsmothers192@gmail.com>
2019-09-19 10:51:31 -0500

    Finished UI for basic parts of the tip calculator

---
+++ javascript/scripts.js
@@ -0,0 +1,7 @@
+//Function that is executed when the button is clicked
+
+document.getElementById("calculateTip").onclick = function(){
+    console.log("Button was clicked");
+}
+
+//This is an anonymous function which is a function without a name
